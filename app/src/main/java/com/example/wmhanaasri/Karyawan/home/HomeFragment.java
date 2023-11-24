d4871f6c01aecc3b907d38464c1b5da37668502f
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-25 06:27:35 +0700

    Delete All not usage and add Tugas

--- app/src/main/java/com/example/wmhanaasri/Karyawan/home/HomeFragment.java
+++ app/src/main/java/com/example/wmhanaasri/Karyawan/home/HomeFragment.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,7 +17,6 @@ import android.widget.TextView;
 
 
 import com.example.wmhanaasri.ListAktivitas;
-import com.example.wmhanaasri.PresensiFragment;
 import com.example.wmhanaasri.R;
 
 import java.text.SimpleDateFormat;
--- app/src/main/java/com/example/wmhanaasri/Karyawan/tugas/TugasFragment.java
+++ app/src/main/java/com/example/wmhanaasri/Karyawan/tugas/TugasFragment.java
@@ -20,58 +20,8 @@ import java.util.ArrayList;
 
 import com.example.wmhanaasri.Karyawan.adapter.AktifitasAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TugasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TugasFragment extends Fragment {
 
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private Spinner spinner;
-    private SpinnerAdapter adapter;
-    private RecyclerView recyclerView;
-    private AktifitasAdapter adapterRecycler;
-    private ArrayList<ListAktivitas> AktifitasArrayList;
-
-    public TugasFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RekapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TugasFragment newInstance(String param1, String param2) {
-        TugasFragment fragment = new TugasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -79,45 +29,7 @@ public class TugasFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.karyawan_fragment_tugas, container, false);
-        spinner = view.findViewById(R.id.spinnerRekap);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(requireActivity(), android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        adapter.add("Tugas");
-        adapter.add("Presensi");
-
-
-        spinner.setAdapter(adapter); // Mengatur adapter ke Spinner
-        spinner.setSelection(0); // Memilih item "Presensi" sebagai item default
-
-        //recycler view
-        recyclerView = view.findViewById(R.id.recycle_viewHome);
-
-        // Membuat objek ArrayList Aktifitas
-        AktifitasArrayList = new ArrayList<ListAktivitas>();
-
-        // Menambahkan data ke ArrayList Aktifitas
-        addData();
-
-        // Membuat dan mengatur adapter
-        adapterRecycler = new AktifitasAdapter(AktifitasArrayList);
-
-        // Membuat dan mengatur layout manager
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity()); // Gunakan getActivity() karena Anda berada dalam fragmen
-
-        // Mengatur layout manager dan adapter untuk RecyclerView
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapterRecycler);
 
         return view;
-    }
-
-    void addData() {
-        AktifitasArrayList = new ArrayList<>();
-        AktifitasArrayList.add(new ListAktivitas("Upload Menu Baru", "Gilang", "14 Oktober 2023"));
-        AktifitasArrayList.add(new ListAktivitas("Upload Menu Baru", "Gilang", "14 Oktober 2023"));
-        AktifitasArrayList.add(new ListAktivitas("Upload Menu Baru", "Gilang", "14 Oktober 2023"));
-        AktifitasArrayList.add(new ListAktivitas("Restok Bahan", "Rizqi", "15 Oktober 2023"));
-        AktifitasArrayList.add(new ListAktivitas("Restok Bahan", "Rizqi", "15 Oktober 2023"));
     }
 }
